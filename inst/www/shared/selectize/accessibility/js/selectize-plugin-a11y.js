758e37f717724d2dc117f6920441d8f1f184723b
Winston Chang <winston@stdout.org>
2020-12-16 17:58:05 -0600

    Apply patches

--- inst/www/shared/selectize/accessibility/js/selectize-plugin-a11y.js
+++ inst/www/shared/selectize/accessibility/js/selectize-plugin-a11y.js
@@ -121,12 +121,6 @@ Selectize.define("selectize-plugin-a11y", function (options) {
       var inputId = self.accessibility.helpers.randomId(),
         listboxId = self.accessibility.helpers.randomId();
 
-      self.$control.on("keydown", function (e) {
-        if (e.keyCode === KEY_RETURN) {
-          $(this).click();
-        }
-      });
-
       self.$control_input.attr({
         role: "combobox",
         "aria-expanded": "false",
